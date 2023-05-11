2424628167d739e7352be579ac0d498c6a0f7d04
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2023-05-11 10:50:46 +0200

    Push de reuperacion

--- Android/MyAplication/app/src/main/java/com/afundacion/myaplication/HomeActivity.java
+++ Android/MyAplication/app/src/main/java/com/afundacion/myaplication/HomeActivity.java
@@ -1,14 +1,22 @@
 package com.afundacion.myaplication;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout drawerLayout;
 
@@ -35,5 +43,40 @@ public class HomeActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int titleId = getTitle(menuItem);
+        showFragment(titleId);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void showFragment(int titleId) {
+        Fragment fragment = HomeFragment.newInstance(titleId);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(R.anim.nav_enter, R.anim.nav_exit)
+                .replace(R.id.home_content, fragment)
+                .commit();
+
+        setTitle(getString(titleId));
+    }
+
+    private int getTitle(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.incio:
+                return 1;
+            case R.id.listado:
+                return 2;
+            case R.id.acerca_de:
+                return 3;
+            default:
+                throw new IllegalArgumentException("menu option not implemented!!");
+        }
+
+
+
     }
 }
